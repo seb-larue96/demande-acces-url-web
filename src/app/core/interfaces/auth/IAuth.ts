009dc4d259aab4ce8610fb3a076e5a0d6b02c3b7
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-24 16:23:37 +0400

    add auth interface

---
+++ src/app/core/interfaces/auth/IAuth.ts
@@ -0,0 +1,11 @@
+export interface loginRequest {
+    email: string;
+    password: string;
+}
+
+export interface registerRequest {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+}
