4c9b874bbb2b9f5ab93f0eea70edf2ece7d0b661
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-25 10:39:16 +0400

    add menu component

---
+++ src/app/features/main/components/menu/menu.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidenav } from '../../../../shared/layout/components/sidenav/sidenav';
+
+@Component({
+  selector: 'app-menu',
+  imports: [Sidenav],
+  templateUrl: './menu.html',
+  styleUrl: './menu.scss',
+})
+export class Menu {
+
+}
