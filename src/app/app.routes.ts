cdf5d466410897b6f58ce234e984b291490ba229
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-25 11:10:03 +0400

    add home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'acceuil',
+        pathMatch: 'full',
+    },
+    {
+        path: 'acceuil',
+        loadComponent: () => import('./features/main/components/home/home').then(c => c.Home)
+    }
+];
