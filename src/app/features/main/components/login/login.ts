3abb7bfd3338761913ec073ffab025494c0d88b5
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-25 09:45:51 +0400

    add login component

---
+++ src/app/features/main/components/login/login.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { LoginModules } from '../../modules/login.module';
+import { loginRequest } from '../../../../core/interfaces/auth/IAuth';
+import { AuthService } from '../../../../core/services/auth/auth-service';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  imports: [LoginModules],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  loginRequest: loginRequest = {
+    email: "",
+    password: ""
+  };
+
+  private authService = inject(AuthService);
+  private toastr = inject(ToastrService);
+
+  login(form: NgForm): void {
+    if(!form.valid) {
+      this.toastr.error("Veuillez remplir tous les champs obligatoires.", "Erreur de validation")
+      return;
+    }
+
+    this.authService.login(this.loginRequest).subscribe({
+      next: () => this.toastr.success("Vous êtes connecté(e) avec succès.", "Bienvenue !"),
+      error: () => this.toastr.error("Échec de la connexion.", "Erreur"),
+    });;
+  }
+}
