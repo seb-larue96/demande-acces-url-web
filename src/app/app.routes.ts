d8a77f7fb8cd53e3fd12596e4d69de81fcb16fca
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-27 09:42:21 +0400

    add access requests route to route and sidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     {
         path: 'acceuil',
         loadComponent: () => import('./features/main/components/home/home').then(c => c.Home)
+    },
+    {
+        path: 'demandes-acces',
+        loadComponent: () => import('./features/access-requests/components/list-access-requests/list-access-requests').then(c => c.ListAccessRequests)
     }
 ];
