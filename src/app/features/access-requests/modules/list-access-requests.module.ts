6479add13e85e1bae3003f41e2f02e0da6d8b2a4
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-27 09:41:30 +0400

    add list access request module

---
+++ src/app/features/access-requests/modules/list-access-requests.module.ts
@@ -0,0 +1,15 @@
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+export const ListAccessRequestsModule = [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule
+]
