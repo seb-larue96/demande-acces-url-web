5123fa08e3aebcec0dab4f413647ee47dd1cd681
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-12-04 14:42:53 +0400

    add role guard to protect routes

---
+++ src/app/core/guards/role.guard.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "../services/auth/auth-service";
+import { ROLE_HIERARCHY } from "../constants/role-hierarchy";
+
+@Injectable({ providedIn: 'root' })
+export class RoleGuard implements CanActivate {
+    private authService = inject(AuthService);
+    private router = inject(Router);
+
+    canActivate(route: ActivatedRouteSnapshot): boolean {
+        const requiredRole = route.data['role'] as string;
+        const userRole = this.authService.role();
+
+        if (!userRole) {
+            this.router.navigate(['/']);
+            return false;
+        }
+
+        const userLevel = ROLE_HIERARCHY[userRole];
+        const requiredLevel = ROLE_HIERARCHY[requiredRole];
+
+        if (userLevel >= requiredLevel) {
+            return true;
+        }
+
+        this.router.navigate(['/']);
+        return false;
+    }
+}
