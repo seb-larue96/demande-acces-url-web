74314a92148a9ee80c22101a85d82316212fab2f
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-12-04 14:44:22 +0400

    add role hierarchy constants

---
+++ src/app/core/constants/role-hierarchy.ts
@@ -0,0 +1,5 @@
+export const ROLE_HIERARCHY: Record<string, number> = {
+  Guest: 1,
+  User: 2,
+  Admin: 3
+};
--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router } from "@angular/router";
 import { AuthService } from "../services/auth/auth-service";
 import { ROLE_HIERARCHY } from "../constants/role-hierarchy";
 
